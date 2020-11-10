3559a15d34931b2a8c6c4b089581e2189b46557f
yui421 <banhgao.nya@gmail.com>
2020-11-10 15:01:18 +0700

    bai3_8 done.

--- KTLT/src/chuong3/bai3_8.java
+++ KTLT/src/chuong3/bai3_8.java
@@ -17,20 +17,30 @@ public class bai3_8 {
 
   // In ra tất cả ước số của n  
   private static void printAllDivisors(int n) {
-    System.out.print("b) Các ước số của n: ");
-
+    int count = 0;
     for (int i = 1; i<=n; ++i) {
-      if (n % i == 0)
+      if (n % i == 0) {
         System.out.print(i + " ");
+        count += 1;
+      }
     }
-
+    System.out.println(", co " + count + " uoc so.");
+  }
+  
+  // In ra tat ca cac uoc so la so nguyen to cua n
+  private static void printAllDivisorIsPrimeNumber(int n) {
+    
+    for (int i = 1; i<=n; ++i) {
+      if (n % i == 0 && isPrimeNumber(i))
+      System.out.print(i + " ");
+    }
     System.out.println();
   }
 
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
-    System.out.println("Nhập n: ");
+    System.out.print("Nhập n: ");
     final int n = input.nextInt();
     // final int n = 4;
 
@@ -42,7 +52,11 @@ public class bai3_8 {
     }
     
     System.out.println("a) " + (isPrimeNumber(n) ? "N là số nguyên tố" : "N không phải số nguyên tố"));
-    printAllDivisors(n);   
-  
+    
+    System.out.print("b) Các ước số của n: ");
+    printAllDivisors(n);
+
+    System.out.print("c) Cac uoc so la so nguyen to cua n: ");
+    printAllDivisorIsPrimeNumber(n);
   }
 }
